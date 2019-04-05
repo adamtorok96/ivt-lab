00b1b6716529f5423b94ede8609d8978762a9487
Adam Torok <adamtorok96@sch.bme.hu>
2019-04-05 08:54:32 +0200

    firing mode all impl.

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,9 +77,17 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
+			boolean primary = false, secondary = false;
+			
+			if( !primaryTorpedoStore.isEmpty() ) {
+				primary = primaryTorpedoStore.fire(1);
+			}
+			
+			if( !secondaryTorpedoStore.isEmpty() ) {
+				secondary = secondaryTorpedoStore.fire(1);
+			}
 
+			firingSuccess = primary && secondary;
         break;
     }
 
